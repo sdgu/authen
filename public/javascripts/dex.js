91779deb402f75e2b9d64fbe3fd7662dc9857fcc
Shawn <sdg18@duke.edu>
2016-01-19 22:41:51 -0500

    dex character info and such

--- public/javascripts/dex.js
+++ public/javascripts/dex.js
@@ -7,19 +7,88 @@ function getByKeyword(event)
 {
 	event.preventDefault();
 
+	var charRes = "";
+	var storyRes = "";
+
+	var charQueryContent = "";
+	var storyQueryContent = "";
+
+	var charDetailContent = "";
+	var storyDetailContent = "";
+
 	$.getJSON("/characters/characterlist", function(data)
 	{
-		var options = 
+
+		$.getJSON("/stories/storylist", function(dataStories)
 		{
-			keys: ["character.name"],
-			id: "character.name"
-		}
+		
+			var query = $("#keywordSearch").val();
+			//alert(query);
 
-		var f = new Fuse(data, options);
-		var result = f.search("test");
+			var options = 
+			{
+				keys: ["character.name"],
+				id: "",
+				threshold: 0.33
+			}
 
-		alert(result);
+			var f = new Fuse(data, options);
+			charRes = f.search(query);
+			
+
+			charQueryContent = "<strong>Characters</strong> <br />";
+
+			charDetailContent += '<table class="variousInfo"><tr><td><h2>Character Info</h2></td></tr>';
+			charDetailContent += '<tr><td>'
+
+			$.each(charRes, function(index, value)
+			{
+				
+				charQueryContent += this.character.name + "<br />";
+
+				charDetailContent += '<div id="charInfo">';
+				charDetailContent += '<table><tr>';
+				charDetailContent += '<td class="titl"><strong>Character Name: </strong></td>';
+				charDetailContent += '<td>' + this.character.name + '</td></tr>'
+				
+
+
+				charDetailContent += '</table>';
+
+
+
+			});
+
+			charDetailContent += '</div></table>';
+
+
+	
+			//alert(query);
+
+			var options2 = 
+			{
+				keys: ["title"],
+				id: "title",
+				threshold: 0.33
+			}
+
+			var f = new Fuse(dataStories, options2);
+			storyRes = f.search(query);
+			storyQueryContent = "<strong>Stories</strong> <br />"
+			$.each(storyRes, function(index, vaule)
+			{
+				storyQueryContent += this + "<br />";
+			});
+
+
+			$("#displayHere").html(charQueryContent + "<br />" + storyQueryContent);
+			$("#displayDetails").html(charDetailContent);
+			//$("#displayHere").html("<strong>Characters</strong> <br />" + charRes.join("<br />") + "<br />" + "<br /> <strong>Stories</strong> <br />" + storyRes.join("<br />"));
+
+		});
 
 	});
+
+
 
 }
