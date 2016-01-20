f31c1fd0aecbf00d1d1b395fb465cbafb0aaa165
Shawn <sdg18@duke.edu>
2016-01-19 23:29:01 -0500

    dex search by keywords

--- public/javascripts/dex.js
+++ public/javascripts/dex.js
@@ -38,51 +38,99 @@ function getByKeyword(event)
 
 			charQueryContent = "<strong>Characters</strong> <br />";
 
-			charDetailContent += '<table class="variousInfo"><tr><td><h2>Character Info</h2></td></tr>';
-			charDetailContent += '<tr><td>'
+			charDetailContent += '<table class="variousInfo">';
+			charDetailContent += '<tr><td><h2>Character Info</h2></td></tr>';
+			charDetailContent += '<tr><td>';
 
 			$.each(charRes, function(index, value)
 			{
 				
-				charQueryContent += this.character.name + "<br />";
+				charQueryContent += '<a href="#' + this.character.name + '">' + this.character.name + '</a><br />';
 
-				charDetailContent += '<div id="charInfo">';
-				charDetailContent += '<table><tr>';
+				charDetailContent += '<div class="charInfo" id="' + this.character.name + '">';
+
+				charDetailContent += '<table>';
+
+				charDetailContent += '<tr>';
 				charDetailContent += '<td class="titl"><strong>Character Name: </strong></td>';
-				charDetailContent += '<td>' + this.character.name + '</td></tr>'
-				
+				charDetailContent += '<td>' + this.character.name + '</td></tr>';
 
+				charDetailContent += '<tr>';
+				charDetailContent += '<td class="titl"><strong>Formes: </strong></td>';
+				charDetailContent += '<td>' + this.character.formes + '</td></tr>';
+
+				charDetailContent += '<tr>';
+				charDetailContent += '<td class="titl"><strong>Abilities: </strong></td>';
+				charDetailContent += '<td>' + this.character.abilities + '</td></tr>';
+
+				charDetailContent += '<tr>';
+				charDetailContent += '<td class="titl"><strong>Description: </strong></td>';
+				charDetailContent += '<td>' + this.character.description + '</td></tr>';
+
+				charDetailContent += '<tr>';
+				charDetailContent += '<td class="titl"><strong>Author: </strong></td>';
+				charDetailContent += '<td>' + this.author + '</td></tr>'
 
 				charDetailContent += '</table>';
 
-
-
+				charDetailContent += '</div>';
+				
 			});
 
-			charDetailContent += '</div></table>';
+			charDetailContent += '</td></tr></table>';
 
-
-	
 			//alert(query);
 
 			var options2 = 
 			{
 				keys: ["title"],
-				id: "title",
+				id: "",
 				threshold: 0.33
 			}
 
 			var f = new Fuse(dataStories, options2);
 			storyRes = f.search(query);
+
 			storyQueryContent = "<strong>Stories</strong> <br />"
+
+			storyDetailContent += '<table class="variousInfo">';
+			storyDetailContent += '<tr><td><h2>Story Info</h2></td></tr>';
+			storyDetailContent += '<tr><td>';
+
 			$.each(storyRes, function(index, vaule)
 			{
-				storyQueryContent += this + "<br />";
+				storyQueryContent += '<a href="#' + this.title + '">' + this.title + "</a><br />";
+
+				storyDetailContent += '<div class="storyInfo" id="' + this.title + '">';
+
+				storyDetailContent += '<table>';
+
+				storyDetailContent += '<tr>';
+				storyDetailContent += '<td class="titl"><strong>Story Title: </strong></td>';
+				storyDetailContent += '<td>' + this.title + '</td></tr>';
+
+				storyDetailContent += '<tr>';
+				storyDetailContent += '<td class="titl"><strong>Characters: </strong></td>';
+				storyDetailContent += '<td>' + this.characters + '</td></tr>';
+
+				storyDetailContent += '<tr>';
+				storyDetailContent += '<td class="titl"><strong>Tags: </strong></td>';
+				storyDetailContent += '<td>' + this.tags + '</td></tr>';
+
+				storyDetailContent += '<tr>';
+				storyDetailContent += '<td class="titl"><strong>Story: </strong></td>';
+				storyDetailContent += '<td>' + this.story + '</td></tr>';
+
+				storyDetailContent += '</table>';
+
+				storyDetailContent += '</div>';
+
 			});
 
+			storyDetailContent += '</td></tr></table>';
 
 			$("#displayHere").html(charQueryContent + "<br />" + storyQueryContent);
-			$("#displayDetails").html(charDetailContent);
+			$("#displayDetails").html(charDetailContent + "<br />" + storyDetailContent);
 			//$("#displayHere").html("<strong>Characters</strong> <br />" + charRes.join("<br />") + "<br />" + "<br /> <strong>Stories</strong> <br />" + storyRes.join("<br />"));
 
 		});
